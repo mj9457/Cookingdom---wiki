b44e4abd4a58adaac43686aae5a78164a48022e5
mj9457 <mj9457@naver.com>
2021-07-08 02:39:51 +0900

    coupon, Modify menu

---
+++ assets/js/coupon.js
@@ -0,0 +1,62 @@
+function copyToClipboard(val) {
+  const t = document.createElement("textarea");
+  document.body.appendChild(t);
+  t.value = val;
+  t.select();
+  document.execCommand('copy');
+  document.body.removeChild(t);
+}
+    function copy() {
+    copyToClipboard('RUSWKGMLKINGDOM6');
+    console.log('복사완료!');
+    }
+
+    function copya() {
+        copyToClipboard('JJONDEUKEECOOKIE');
+        console.log('복사완료!');
+    }
+      
+    function copyb() {
+        copyToClipboard('COOSEBOMEKINGDOM');
+        console.log('복사완료!');
+    }
+
+    function copyc() {
+        copyToClipboard('COOKANGJIKINGDOM');
+        console.log('복사완료!');
+    }
+
+    function copyd() {
+        copyToClipboard('DOMINOCOOKINGDOM');
+        console.log('복사완료!');
+    }
+
+    function copye() {
+        copyToClipboard('COOKIECHICKENRUN');
+        console.log('복사완료!');
+    }
+
+    function copyf() {
+        copyToClipboard('GETUR7SUGARGNOME');
+        console.log('복사완료!');
+    }
+
+    function copyg() {
+        copyToClipboard('GETUR5SUGARGNOME');
+        console.log('복사완료!');
+    }
+
+    function copyh() {
+        copyToClipboard('GETUR3SUGARGNOME');
+        console.log('복사완료!');
+    }
+
+    function copyi() {
+        copyToClipboard('GETUR1SUGARGNOME');
+        console.log('복사완료!');
+    }
+
+    function coupon() {
+        copyToClipboard('www.naver.com');
+        console.log('복q사완료!');
+    }
